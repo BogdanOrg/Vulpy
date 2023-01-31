6798a4743894d99246923c0d9046f63c9c65977e
bogdan <bogdan.ganzha@mend.io>
2023-01-31 15:08:37 +0100

    add new high, fix medium

--- bad/libuser.py
+++ bad/libuser.py
@@ -50,7 +50,8 @@ def password_change(username, password):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
+    c.execute("UPDATE users SET password = WHERE username =")
+    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
     conn.commit()
 
     return True
